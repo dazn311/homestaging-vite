a0eda2028f463ff6907c5c33d577bb993aceaeab
Alexandr Ryzhenkov <alex2505@bk.ru>
2025-11-22 15:05:16 +0300

    add About block [251122-1505];

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Header} from "./components/Header.tsx";
 import {Title} from "./components/Title.tsx";
+import {About} from "./components/About.tsx";
 
 function App() {
   return (
     <>
       <Header/>
       <Title/>
+      <About/>
       <h1>Vite + React</h1>
     </>
   )
