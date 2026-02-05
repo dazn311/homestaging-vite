c177b36a8469a59260b5373d48d6a0fbca2a82dd
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-05 09:51:01 +0300

    upd Cooperation [260205-0950];
    [
     Cooperation
    
    ]

--- src/components/cooperation/Cooperation.tsx
+++ src/components/cooperation/Cooperation.tsx
@@ -1,3 +1,5 @@
+import {ButtonTelegram} from "../btn-telegram/ButtonTelegram.tsx";
+
 export const Cooperation = () => {
 
   return (
@@ -8,8 +10,8 @@ export const Cooperation = () => {
           <div className="col-xl-10">
             <div className="text-center">
               <h3>Интересное</h3>
-              <p>Все дополнительные ссылки и живое общение на моем Телеграм канале</p>
-              <a className="cta-btn" href="https://t.me/homeupakovka"><i className="bi bi-telegram"></i>Перейти</a>
+              <p>Все дополнительные ссылки, проекты в реальном времени, живое общение на моем Телеграм канале</p>
+              <ButtonTelegram title={'Перейти'} cls={'telegram'} cls1={'cta-btn'}/>
             </div>
           </div>
         </div>
