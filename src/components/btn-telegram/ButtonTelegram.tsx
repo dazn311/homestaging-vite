27d9ceb9ae9a89656c71f2889caded473a39b198
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-05 09:50:44 +0300

    upd ButtonTelegram [260205-0950];
    [
     ButtonTelegram
    
    ]

--- src/components/btn-telegram/ButtonTelegram.tsx
+++ src/components/btn-telegram/ButtonTelegram.tsx
@@ -3,11 +3,12 @@ import cn from "classnames";
 export const ButtonTelegram = (
   {
     title='',
-    cls='telegram'
-  }:{title?:string;cls?:string;}) => {
+    cls1='buy-btn',
+    cls='telegram',
+  }:{title?:string;cls1?:string;cls?:string;}) => {
   return (
     <div className="text-center">
-      <a href="https://t.me/homeupakovka" className="buy-btn">
+      <a href="https://t.me/homeupakovka" className={cls1}>
         <i className={cn('bi','p-1',`bi-${cls}`)}/>{title}
       </a>
     </div>
