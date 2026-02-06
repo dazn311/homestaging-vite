8b903aa2f56c6348f5b944c99184de242456294b
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-06 15:43:13 +0300

    upd on click ImageBlock [260206-1543];
    [
    
     ImageBlock
    ]

--- src/components/portfolio/ImageBlock.tsx
+++ src/components/portfolio/ImageBlock.tsx
@@ -1,24 +1,49 @@
-// import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {type TImageBlockData} from "../../store/dataApp.ts";
-// import { Button, Image, InputNumber } from 'antd';
+import {Image} from 'antd';
 
 //used in Portfolio;
 export function ImageBlock({docId,title,price,imageUrl}:TImageBlockData) {
+  const [open, setOpen] = useState(false);
   const urlDoc = `/?details=${docId}`;
+
+  const onClickHandler = (event: React.MouseEvent<HTMLElement>) => {
+    // @ts-ignore
+    const localName = event.target.localName ?? '';
+    if (/svg/.test(localName)) {
+      return;
+    }
+    event.preventDefault();
+    setOpen(!open);
+  }
+
   return (
     <div className="col-lg-4 col-md-6 portfolio-item isotope-item filter-ilinskie-luga-21">
-      <div className="div-bg" style={{
-        backgroundImage: `url(${imageUrl})`
-      }}>
-        {/*<Image*/}
-        {/*  className="img-fluid"*/}
-        {/*  alt={title}*/}
-        {/*  src={imageUrl}*/}
-        {/*/>*/}
+      <div
+        className="div-bg"
+        onClick={onClickHandler}
+        style={{backgroundImage: `url(${imageUrl})`}}>
+        <Image
+          style={{ display: 'none' }}
+          alt={title}
+          src={imageUrl}
+          preview={{
+            open,
+            scaleStep:0.5,
+            src: imageUrl,
+            onOpenChange: (value) => {
+              setOpen(value);
+            },
+          }}
+        />
       </div>
       <div className="portfolio-info">
         <h4>{title}</h4>
-        <PriceBlock price={price} />
+        <PriceBlock price={price}/>
+        <a href={imageUrl} onClick={onClickHandler} title="Увеличить"
+           data-gallery="portfolio-gallery-rimskogoKorsakova9152" className="glightbox preview-link">
+          <i className="bi bi-zoom-in"></i>
+        </a>
         <a href={urlDoc} title={`перейти на страницу ${title}`}
            className="details-link">
           <i className="bi bi-link-45deg"></i>
@@ -28,10 +53,10 @@ export function ImageBlock({docId,title,price,imageUrl}:TImageBlockData) {
   )
 }
 
-function PriceBlock({price}: { price:number }) {
-  const isBigPrice= price >= 1000000;
-  const priceRes = isBigPrice ? (price/1000000).toFixed(2)
-    :(price/1000).toFixed();
+function PriceBlock({price}: { price: number }) {
+  const isBigPrice = price >= 1000000;
+  const priceRes = isBigPrice ? (price / 1000000).toFixed(2)
+    : (price / 1000).toFixed();
   const mln = isBigPrice ? 'млн' : 'тыс';
   return (
     <p>бюджет {priceRes} {mln}. руб</p>
