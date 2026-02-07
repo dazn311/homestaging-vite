5271d4f3e88029f9281fe85827c7bc32e50af9a4
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-07 17:54:06 +0300

    upd styles  NoFinded [260207-1753];
    [
    
     NoFinded
     no-finded.styles.scss
    ]

--- src/pages/no-finded/NoFinded.tsx
+++ src/pages/no-finded/NoFinded.tsx
@@ -14,7 +14,7 @@ export const NoFinded = () => {
       </div>
 
       <div className="info">
-        <h2>We can't find that page</h2>
+        <h2>Мы не нашли страницу</h2>
         <p>We're fairly sure that page used to be here, but seems to have gone missing. We do apologise on it's behalf.</p>
         <Link to={'/'} rel={'noreferrer noopener'}>Home</Link>
       </div>
