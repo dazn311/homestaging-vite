1d71079103d5f6a5d51d6562211ef6ddb6fafa6d
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-09 19:10:43 +0300

    add open menu NavHeader [260209-1910];
    [
     NavHeader
    ]

--- src/components/navHeader/NavHeader.tsx
+++ src/components/navHeader/NavHeader.tsx
@@ -2,10 +2,20 @@ import {Link} from "react-router";
 import {navmenuArr, navmenuSubArr} from "./dataNav.ts";
 import {ListItem} from "./ListItem.tsx";
 import './nav.scss';
+import {useState} from "react";
+import cn from "classnames";
 
 export const NavHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  function handlerClick(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsOpen(prev =>!prev);
+  }
+
   return (
-    <div className="branding d-flex align-items-center">
+    <div onClick={()=>setIsOpen(false)} className={cn("branding d-flex align-items-center",
+      {'mobile-nav-active': isOpen})}>
       <div className="container position-relative d-flex align-items-center justify-content-between">
         <Link to="/" className="logo d-flex align-items-center">
           <h1 className="sitename">ХоумУпаковка МОСКВА</h1>
@@ -16,10 +26,10 @@ export const NavHeader = () => {
               <li key={item.url}><a href={item.url}>{item.title}</a></li>
             ))}
             <li className="dropdown text-secondary">
-              <a>
+              <Link to={'/projects?jkName=all'}>
                 <span className="text-secondary">Проекты</span>
                 <i className="bi bi-chevron-down toggle-dropdown"></i>
-              </a>
+              </Link>
               <ul className={'jk-name'}>
                 {navmenuSubArr.map((item,idx) => (
                   <ListItem key={'nav'+idx} item={item} />
@@ -28,7 +38,7 @@ export const NavHeader = () => {
             </li>
             <li><Link to={"/#contact"}>Контакты</Link></li>
           </ul>
-          <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          <i onClick={handlerClick} className="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
       </div>
     </div>
