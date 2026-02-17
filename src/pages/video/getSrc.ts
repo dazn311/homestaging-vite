09e5157ccfca364bd799e5017cd513ef6e1e7e21
Alexandr Ryzhenkov <alex2505@bk.ru>
2026-02-17 11:13:07 +0300

    upd url ver getSrc [260217-1113];
    [
     getSrc
    ]

--- src/pages/video/getSrc.ts
+++ src/pages/video/getSrc.ts
@@ -4,5 +4,5 @@ import {baseUrl, type TVideoCard} from "../../api/data-video.ts";
 export function getSrc(item: TVideoCard,type?:string) {
 
   const typeDoc = type || 'png';
-  return encodeURI(`${baseUrl}/${item.video}.${typeDoc}`);
+  return encodeURI(`${baseUrl}/${item.video}.${typeDoc}?ver=2`);
 }
