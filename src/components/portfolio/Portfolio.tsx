9b82148bedc9660dc6d225d11a5601b65b9462b7
Alexander Ryzhenkov <alex2505@bk.ru>
2026-02-18 09:02:43 +0300

    upd AnimatePresence [260218-0902]

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,15 +1,21 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {ImageBlockData} from "../../store/dataApp.ts";
 import {ImageBlock} from "./ImageBlock.tsx";
 import {PortfolioMenu} from "./PortfolioMenu.tsx";
 import {TitleBlock} from "../title-block/TitleBlock.tsx";
 import {motion, AnimatePresence} from "motion/react";
+import {useIsVisible} from "../../hooks/useIsVisible.tsx";
 
 export const Portfolio = () => {
   const [activeDocId, setActiveDocId] = useState('kronstadskii8k2p2');
+  const ref = useRef(null);
+  const isVisible = useIsVisible(ref);
 
   return (
-    <section id="portfolio-anchor" className="portfolio section">
+    <section
+      id="portfolio-anchor"
+      ref={ref}
+      className="portfolio section">
       <TitleBlock title={'Портфолио проектов'}/>
       <div className="container">
         <div className="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
@@ -18,22 +24,25 @@ export const Portfolio = () => {
             className="row gy-4 isotope-container"
             style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}
           >
-            <AnimatePresence>
-              {ImageBlockData
+            <AnimatePresence mode={'wait'}>
+              {isVisible && ImageBlockData
                 .filter(im => {
                   return im.docId === activeDocId || /^\*$/.test(activeDocId);
                 })
-                .map((item,idx) => (
-                  <motion.div
-                    initial={{ x: -200, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
-                    exit={{ x: '100vw', opacity: 0 }}
-                    transition={{delay: idx * 0.2, duration: .5}}
-                    className={'div-bg'}
-                    key={item.id}
-                  >
-                    <ImageBlock key={item.id} {...item} />
-                  </motion.div>))}
+                .map((item,idx) => {
+                  const delay = isVisible ? (idx * 0.2) : 1;
+                  return (
+                    <motion.div
+                      initial={{ x: -200, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      exit={{ x: '100vw', opacity: 0 }}
+                      transition={{delay: delay, duration: .5}}
+                      className={'div-bg'}
+                      key={item.id}
+                    >
+                      <ImageBlock key={item.id} {...item} />
+                    </motion.div>)
+                })}
             </AnimatePresence>
           </div>
         </div>
