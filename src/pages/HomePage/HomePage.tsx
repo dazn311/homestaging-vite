dadb9cc93ddca42fb2e21b22e8985acee57d96d2
Alexander Ryzhenkov <alex2505@bk.ru>
2026-02-19 16:55:12 +0300

    upd Projects [260219-1655]
    [
      Projects
    ]

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,15 @@
 import {useEffect} from "react";
 import {useLocation} from "react-router";
-import {Title} from "../components/Title.tsx";
-import {About} from "../components/about/About.tsx";
-import {Services} from "../components/services/Services.tsx";
-import {Prices} from "../components/prices/Prices.tsx";
-import {Cooperation} from "../components/cooperation/Cooperation.tsx";
-import {Portfolio} from "../components/portfolio/Portfolio.tsx";
-import {Contact} from "../components/contact/Contact.tsx";
-import TimelineComp from "../components/timeline/TimelineComp.tsx";
-import {ScrollTopBtn} from "../components/scroll-top-btn/ScrollTopBtn.tsx";
-import './App.css';
+import {Title} from "../../components/Title.tsx";
+import {About} from "../../components/about/About.tsx";
+import {Services} from "../../components/services/Services.tsx";
+import {Prices} from "../../components/prices/Prices.tsx";
+import {Cooperation} from "../../components/cooperation/Cooperation.tsx";
+import {Portfolio} from "../../components/portfolio/Portfolio.tsx";
+import {Contact} from "../../components/contact/Contact.tsx";
+import TimelineComp from "../../components/timeline/TimelineComp.tsx";
+import {ScrollTopBtn} from "../../components/scroll-top-btn/ScrollTopBtn.tsx";
+import '../../app/App.css';
 
 function HomePage() {
   const location = useLocation();
